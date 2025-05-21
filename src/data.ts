1aaab1247eb3584495b19766ac0d2b1443aedd3b
arafat-alim <arafat97alim@gmail.com>
2025-05-22 00:31:41 +0530

    ✅ Path, data, and Rootlayout updated.

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const initialData = [
+  {
+    id: 1,
+    content: "This is the first ticket",
+    status: "DONE",
+  },
+  {
+    id: 2,
+    content: "This is the second ticket",
+    status: "OPEN",
+  },
+  {
+    id: 3,
+    content: "This is the third ticket",
+    status: "WORK",
+  },
+];
