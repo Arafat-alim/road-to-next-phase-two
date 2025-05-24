530062ccd0e532f88f8a2df61e29a78acdc3700c
arafat-alim <arafat97alim@gmail.com>
2025-05-24 14:26:40 +0530

    ✅ Header custom component added and implemented.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import "./globals.css";
 
-import { LucideKanbanSquare } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 
-import { Button, buttonVariants } from "@/components/ui/button";
-import { homePath, ticketsPath } from "@/path";
+import { Header } from "@/components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,26 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
-          <div>
-            <Button asChild variant={"ghost"}>
-              <Link href={homePath()}>
-                <LucideKanbanSquare />
-                <h1 className="ml-1 text-lg font-semibold">TicketBounty</h1>
-              </Link>
-            </Button>
-          </div>
-          <div>
-            {/* <Button>  asChild variant={"outline"} */}
-            <Link
-              href={ticketsPath()}
-              className={buttonVariants({ variant: "default" })}
-            >
-              <span className="text-lg font-medium">Tickets</span>
-            </Link>
-            {/* </Button> */}
-          </div>
-        </nav>
+        <Header />
         <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
           {children}
         </main>
---
+++ src/components/header.tsx
@@ -0,0 +1,34 @@
+import { LucideKanbanSquare } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+import { homePath, ticketsPath } from "@/path";
+
+import { Button, buttonVariants } from "./ui/button";
+
+const Header = () => {
+  return (
+    <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
+      <div>
+        <Button asChild variant={"ghost"}>
+          <Link href={homePath()}>
+            <LucideKanbanSquare />
+            <h1 className="ml-1 text-lg font-semibold">TicketBounty</h1>
+          </Link>
+        </Button>
+      </div>
+      <div>
+        {/* <Button>  asChild variant={"outline"} */}
+        <Link
+          href={ticketsPath()}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <span className="text-lg font-medium">Tickets</span>
+        </Link>
+        {/* </Button> */}
+      </div>
+    </nav>
+  );
+};
+
+export { Header };
