0afcb87645b43a73d010e84acc77f06051b14545
arafat-alim <arafat97alim@gmail.com>
2025-05-24 12:56:15 +0530

    ✅ Lucide React icons implemented.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
 import { homePath, ticketsPath } from "@/path";
+import { LucideKanbanSquare } from "lucide-react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,11 @@ export default function RootLayout({
       >
         <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div>
-            <Button asChild variant={"outline"}>
-              <Link href={homePath()}>Home</Link>
+            <Button asChild variant={"ghost"}>
+              <Link href={homePath()}>
+                <LucideKanbanSquare />
+                <h1 className="ml-1 text-lg font-semibold">TicketBounty</h1>
+              </Link>
             </Button>
           </div>
           <div>
