fa28c064baff32e0f64f86e4c52e7390be200966
arafat-alim <arafat97alim@gmail.com>
2025-05-26 22:14:29 +0530

    ✅Supabase and Prisma implementation.

---
+++ prisma/seed.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+const tickets = [
+  {
+    title: "Ticket 1",
+    content:
+      "This is the first ticket Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    status: "DONE" as const,
+  },
+  {
+    title: "Ticket 2",
+    content:
+      "This is the second ticket. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    status: "OPEN" as const,
+  },
+  {
+    title: "Ticket 3",
+    content:
+      "This is the third ticket, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    status: "IN_PROGRESS" as const,
+  },
+];
+
+const seed = async () => {
+  //   for (const ticket of tickets) {
+  //     await prisma.ticket.create({
+  //       data: ticket,
+  //     });
+  //   }
+  //   const promises = tickets.map((ticket) =>
+  //     prisma.ticket.create({
+  //       data: ticket,
+  //     })
+  //   );
+  //   await Promise.all(promises);
+  console.log("⚙️ Database seed: Started...");
+
+  const t0 = performance.now();
+
+  await prisma.tickets.deleteMany();
+
+  await prisma.tickets.createMany({
+    data: tickets,
+  });
+  const t1 = performance.now();
+
+  console.log(`🎊 Database seed: Finished ${t1 - t0}ms`);
+};
+
+seed();
