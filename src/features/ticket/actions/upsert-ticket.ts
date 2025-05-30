f415e603a24ccbba52eba96f76b275aa5d434878
arafat-alim <arafat97alim@gmail.com>
2025-05-30 19:49:32 +0530

    ✅ Error handling implemented.

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -3,6 +3,10 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
+import {
+  ActionState,
+  fromErrorToActionState,
+} from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/path";
 
@@ -13,7 +17,7 @@ const upsertTicketSchema = z.object({
 
 export const upsertTicket = async (
   id: string | undefined,
-  _actionState: { message: string; payload?: FormData },
+  _actionState: ActionState,
   formData: FormData
 ) => {
   try {
@@ -29,12 +33,8 @@ export const upsertTicket = async (
       update: data,
       create: data,
     });
-  } catch (err) {
-    console.error("error: ", err);
-    return {
-      message: "Something went wrong",
-      payload: formData,
-    };
+  } catch (error) {
+    return fromErrorToActionState(error, formData);
   }
 
   revalidatePath(ticketsPath());
