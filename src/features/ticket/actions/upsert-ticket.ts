4529a98784e3c457897968bfc8eda304b6f0a4ad
arafat-alim <arafat97alim@gmail.com>
2025-05-30 18:21:21 +0530

    ✅ zod implemented.

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -4,24 +4,36 @@ import { redirect } from "next/navigation";
 
 import { prisma } from "@/lib/prisma";
 import { ticketPath, ticketsPath } from "@/path";
+import { z } from "zod";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(170),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title"),
+      content: formData.get("content"),
+    });
 
-  await prisma.tickets.upsert({
-    where: {
-      id: id || "",
-    },
-    update: data,
-    create: data,
-  });
+    await prisma.tickets.upsert({
+      where: {
+        id: id || "",
+      },
+      update: data,
+      create: data,
+    });
+  } catch (err) {
+    return {
+      message: "Something went wrong",
+    };
+  }
 
   revalidatePath(ticketsPath());
 
