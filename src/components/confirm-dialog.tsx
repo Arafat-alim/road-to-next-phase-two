03a78b291fed314e3062968549eeece147732dae
arafat-alim <arafat97alim@gmail.com>
2025-06-05 22:46:38 +0530

    ✅ Confirm dialog has been fixed.

--- src/components/confirm-dialog.tsx
+++ src/components/confirm-dialog.tsx
@@ -1,4 +1,4 @@
-import React, { cloneElement, useState } from "react";
+import React, { cloneElement, useActionState, useState } from "react";
 
 import {
   AlertDialog,
@@ -11,8 +11,9 @@ import {
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 
-import { ActionState } from "./form/utils/to-action-state";
-import { Button } from "./ui/button";
+import { Form } from "./form/form";
+import { SubmitButton } from "./form/submit-button";
+import { ActionState, EMPTY_ACTION_STATE } from "./form/utils/to-action-state";
 
 type UseConfirmDialogProps = {
   title?: string;
@@ -29,9 +30,15 @@ const useConfirmDialog = ({
 }: UseConfirmDialogProps) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const [actionState, formAction] = useActionState(action, EMPTY_ACTION_STATE);
+
   const dialogTrigger = cloneElement(trigger, {
     onClick: () => setIsOpen((prevState) => !prevState),
   });
+
+  const handleSubmit = () => {
+    setIsOpen(false);
+  };
 
   const dialog = (
     <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
@@ -43,9 +50,13 @@ const useConfirmDialog = ({
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction asChild>
-            <form action={action}>
-              <Button type="submit">Confirm</Button>
-            </form>
+            <Form
+              actionState={actionState}
+              action={formAction}
+              onSuccess={handleSubmit}
+            >
+              <SubmitButton label="Confirm" />
+            </Form>
           </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -4,15 +4,20 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { setCookieByKey } from "@/actions/cookies";
+import { fromErrorToActionState } from "@/components/form/utils/to-action-state";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/path";
 
 export const deleteTicket = async (ticketId: string) => {
-  await prisma.tickets.delete({
-    where: {
-      id: ticketId,
-    },
-  });
+  try {
+    await prisma.tickets.delete({
+      where: {
+        id: ticketId,
+      },
+    });
+  } catch (error) {
+    return fromErrorToActionState(error);
+  }
 
   revalidatePath(ticketsPath());
   setCookieByKey("toast", "Ticket deleted!");
