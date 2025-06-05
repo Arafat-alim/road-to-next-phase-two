17877690e80b61d870a11d74d4d6087f7e75e902
arafat-alim <arafat97alim@gmail.com>
2025-06-06 00:13:02 +0530

    ✅ Environment variable file added for defining the base url.

---
+++ src/utils/url.ts
@@ -0,0 +1,12 @@
+const getbaseUrl = () => {
+  const environment = process.env.NODE_ENV;
+
+  const baseUrl =
+    environment === "development"
+      ? "http://localhost:3000"
+      : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+
+  return baseUrl;
+};
+
+export { getbaseUrl };
