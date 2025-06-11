d831d17bec584418c5e2758860e546441dce1b94
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-06-11 19:34:53 +0530

    ✅ Tickets and account page are now protected.

---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+
+export default async function AuthenticatedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  await getAuthOrRedirect();
+
+  return <>{children}</>;
+}
